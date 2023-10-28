2da9e8f25a79f84a00f0cc396c00d6ec79a863a6
Mohammad Mosavat <42.octmst@gmail.com>
2023-10-28 17:09:20 +0330

    fix the problem of styles and added some weathers like wind , and somthing like that

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,45 @@
 const apiKey = "0cb34ba0fb4d7f9ae8a9ad6c30fec97f";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
+const weatherMain = document.getElementById("weatherMain");
+const weatherDesc = document.getElementById("weatherDesc");
 
 const WeatherChecking = async (city) => {
   const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
   const data = await res.json();
   console.log(data);
-  if (data.status === 404) {
-    document.getElementById("error").classList = "inline-block";
-    document.getElementById("weather").classList = "none";
-  } else if (data.status === 404){
+  console.log(document.getElementById("error"));
+
+  if (Number(data.cod) == 404) {
+    document.getElementById("error").style.display = "block";
+    document.getElementById("weather").style.display = "none";
+    console.log("if run");
+  } else {
+    console.log("else run");
     document.getElementById("error").style.display = "none";
-    document.getElementById("weather").style.display= "flex";
+    document.getElementById("weather").style.display = "flex";
     document.getElementById("city").innerHTML = data.name;
     document.getElementById("temp").innerHTML = data.main.temp + "°C";
-    document.getElementById('wind').innerHTML = data.wind.speed + 'km/h'
-    document.getElementById('country').innerHTML = data.sys.country
-    document.getElementById('humidity').innerHTML = data.main.humidity + "%"
+    document.getElementById("wind").innerHTML = data.wind.speed + "km/h";
+    document.getElementById("country").innerHTML = data.sys.country;
+    document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+    
+    weatherMain.innerHTML = data.weather[0].main;
 
     if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/clouds.png?raw=true"
+      weatherDesc.src = "/assets/svgs/cloud-icon.svg";
     } else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/clear.png?raw=true"
+      weatherDesc.src = "/assets/svgs/day-sunny-icon.svg";
     } else if (data.weather[0].main == "Rain") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/rain.png?raw=true"
+      weatherDesc.src = "/assets/svgs/cloud-rain-icon.svg";
     } else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/drizzle.png?raw=true"
+      weatherDesc.src = "/assets/svgs/cloud-rain-lightning-icon.svg";
     } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/mist.png?raw=true"
+      weatherDesc.src = "/assets/svgs/cloud-fog-icon.svg";
     } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "https://github.com/Khyes/12_weather_webapp/blob/main/img/snow.png?raw=true"
+      weatherDesc.src = "/assets/svgs/snow-icon.svg";
     }
   }
 };
@@ -39,6 +48,12 @@ console.log(searchBtn);
 searchBtn.addEventListener("click", () => {
   if (searchInput.value) {
     console.log(searchInput.value);
-    WeatherChecking(searchInput.value)
+    WeatherChecking(searchInput.value);
+  }
+});
+
+searchInput.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13 && searchInput.value) {
+    WeatherChecking(searchInput.value);
   }
 });
